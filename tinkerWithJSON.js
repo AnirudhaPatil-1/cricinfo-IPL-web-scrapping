c1c39f7e66b2675da6853f500d883059d613df68
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-01-01 22:56:04 +0530

    xlsx commands

--- tinkerWithJSON.js
+++ tinkerWithJSON.js
@@ -5,23 +5,23 @@ let xlsx = require("xlsx");
 // console.log("---------------------------");
 // let data = JSON.parse(buffer);
 // console.log(data);
-// let data = require("./example.json");
-// console.log(data);
-// data.push(
-//     {
-//         "name":"Thor",
-//         "last name":"Rogers",
-//         "isAvenger":true,
-//         "friens":["Bruce","Neter","Natasha"],
-//         "age":45,
-//         "address":{
-//             "city":"New York",
-//             "state":"Manhattan"
-//         }
-//     }
-// );
-// let stringedData =  JSON.stringify(data);
-// fs.writeFileSync("./example.JSON", stringedData);
+let data = require("./example.json");
+console.log(data);
+data.push(
+    {
+        "name":"Thor",
+        "last name":"Rogers",
+        "isAvenger":true,
+        "friends":["Bruce","Neter","Natasha"],
+        "age":45,
+        "address":{
+            "city":"New York",
+            "state":"Manhattan"
+        }
+    }
+);
+let stringedData =  JSON.stringify(data);
+fs.writeFileSync("./example.JSON", stringedData);
 
 //XLSX EXCEL COMMANDS
 //creates new workbook
@@ -33,7 +33,23 @@ let xlsx = require("xlsx");
 // //filePath
 // xlsx.writeFile(newWB, filePath);
 
-let newWB = xlsx.utils.book_new();
-let newWS = xlsx.utils.json_to_sheet(data);
-xlsx.utils.book_append_sheet(newWB, newWS, "sheet-1");
-xlsx.writeFile(newWB, "abx.xlsx");
+// let newWB = xlsx.utils.book_new();
+// let newWS = xlsx.utils.json_to_sheet(data);
+// xlsx.utils.book_append_sheet(newWB, newWS, "sheet-1");
+// xlsx.writeFile(newWB, "abc.xlsx");
+
+function excelWriter(filePath, json, sheetName){
+    let newWB = xlsx.utils.book_new();
+    let newWS = xlsx.utils.json_to_sheet(json);
+    xlsx.utils.book_append_sheet(newWB, newWS, sheetName);
+    xlsx.writeFileSync(newWB, "abc.xlsx");
+}
+//READ 
+//get worksheet
+let wb = xlsx.readFile("abc.xlsx");
+//get sheet
+let excelData = wb.Sheets["sheet-1"];
+//get sheet data
+let ans = xlsx.utils.sheet_to_json(excelData);
+console.log(ans);
+
